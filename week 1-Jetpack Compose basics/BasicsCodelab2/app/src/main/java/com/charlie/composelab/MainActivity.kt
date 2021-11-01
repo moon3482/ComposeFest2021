84fd4aaaaba62733da443c3af37c58b867b36e2c
MoonChangKyu <moon3482@naver.com>
2021-11-01 19:42:45 +0900

    Row에 Column Button 적용

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/charlie/composelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/charlie/composelab/MainActivity.kt
@@ -3,14 +3,16 @@ package com.charlie.composelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
@@ -30,21 +32,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary,modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Column(modifier = Modifier.padding(24.dp).fillMaxWidth()) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
 
-            Text(text = "Hello, ")
-            Text(text = name)
+                Text(text = "Hello, ")
+                Text(text = name)
 
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
         }
+
     }
 
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World","Compose")) {
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names){
+        for (name in names) {
             Greeting(name = name)
         }
     }
