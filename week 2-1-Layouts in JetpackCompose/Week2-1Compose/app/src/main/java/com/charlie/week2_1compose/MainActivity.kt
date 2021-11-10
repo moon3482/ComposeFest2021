81c896de9114975fc48c32aea925761fa74b5140
MoonChangKyu <moon3482@naver.com>
2021-11-10 19:29:29 +0900

    Row 에 Modifier 적용

--- week 2-1-Layouts in JetpackCompose/Week2-1Compose/app/src/main/java/com/charlie/week2_1compose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Week2-1Compose/app/src/main/java/com/charlie/week2_1compose/MainActivity.kt
@@ -3,16 +3,20 @@ package com.charlie.week2_1compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +29,8 @@ class MainActivity : ComponentActivity() {
             Week21ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+                    PhotographerCard()
                 }
             }
         }
@@ -46,8 +51,15 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun PhotographerCard() {
-    Row {
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable { }
+            .padding(16.dp)
+            ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
@@ -56,7 +68,9 @@ fun PhotographerCard() {
 
         }
 
-        Column(modifier = Modifier.padding(start = 8.dp).align(Alignment.CenterVertically)) {
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)) {
             Text("Alfred Sisley", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 Text("3 minutes ago", style = MaterialTheme.typography.body2)
@@ -70,6 +84,5 @@ fun PhotographerCard() {
 fun PhotographerCardPreview() {
     Week21ComposeTheme {
         PhotographerCard()
-
     }
 }
