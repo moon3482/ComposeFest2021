5710c598dd59e548c986b1e67716057d64456baf
MoonChangKyu <moon3482@naver.com>
2021-11-17 21:51:18 +0900

    Chip & ChipPreview 만들기

--- week 2-1-Layouts in JetpackCompose/Week2-1Compose/app/src/main/java/com/charlie/week2_1compose/LayoutsCodelab.kt
+++ week 2-1-Layouts in JetpackCompose/Week2-1Compose/app/src/main/java/com/charlie/week2_1compose/LayoutsCodelab.kt
@@ -1,8 +1,21 @@
 package com.charlie.week2_1compose
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.charlie.week2_1compose.ui.theme.Week21ComposeTheme
 
 @Composable
 fun StaggeredGrid(modifier: Modifier = Modifier, rows: Int = 3, content: @Composable () -> Unit) {
@@ -44,6 +57,39 @@ fun StaggeredGrid(modifier: Modifier = Modifier, rows: Int = 3, content: @Compos
         }
     }
 }
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.error)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview(){
+    Week21ComposeTheme() {
+        Chip(text = "Hi there")
+        
+    }
+}
+
 
 
 
