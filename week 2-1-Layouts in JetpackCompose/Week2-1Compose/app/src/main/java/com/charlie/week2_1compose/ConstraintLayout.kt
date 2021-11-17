cd16fb1ee25da432d6e93a845b82a087ebe51906
MoonChangKyu <moon3482@naver.com>
2021-11-17 23:54:58 +0900

    TwoTexts 생성

--- week 2-1-Layouts in JetpackCompose/Week2-1Compose/app/src/main/java/com/charlie/week2_1compose/ConstraintLayout.kt
+++ week 2-1-Layouts in JetpackCompose/Week2-1Compose/app/src/main/java/com/charlie/week2_1compose/ConstraintLayout.kt
@@ -1,10 +1,14 @@
 package com.charlie.week2_1compose
 
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
@@ -75,7 +79,44 @@ fun DecoupledConstraintLayout() {
         }
     }
 
+}
 
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier) {
+        Text(
+            text = text1,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start)
+        )
+        Divider(
+            color = Color.Black, modifier = Modifier
+                .fillMaxHeight()
+                .width(1.dp)
+        )
+
+        Text(
+            text = text2, modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(
+                    Alignment.End
+                )
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview(){
+    Week21ComposeTheme() {
+        Surface() {
+            TwoTexts(text1 = "hi", text2 = "there")
+        }
+    }
 }
 
 private fun decoupledConstraints(margin: Dp): ConstraintSet {
