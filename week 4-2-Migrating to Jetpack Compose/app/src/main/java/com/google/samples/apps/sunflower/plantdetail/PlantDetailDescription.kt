f08a9f0bd22b391970244065e40947fda177a1b5
MoonChangKyu <moon3482@naver.com>
2021-11-28 13:59:31 +0900

    Testing

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
@@ -38,6 +39,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -102,14 +104,7 @@ fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
         PlantDetailContent(it)
     }
 }
-@Preview
-@Composable
-private fun PlantDetailContentPreview() {
-    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
-    MaterialTheme {
-        PlantDetailContent(plant)
-    }
-}
+
 
 @Composable
 fun PlantDetailContent(plant: Plant) {
@@ -119,13 +114,6 @@ fun PlantDetailContent(plant: Plant) {
             PlantWatering(plant.wateringInterval)
             PlantDescription(plant.description)
         }
-    }
-}
-@Preview
-@Composable
-private fun PlantWateringPreview() {
-    MaterialTheme {
-        PlantWatering(7)
     }
 }
 
@@ -152,8 +140,45 @@ private fun PlantDescription(description: String) {
 
 @Preview
 @Composable
+private fun PlantNamePreview() {
+    MdcTheme {
+        PlantName("Apple")
+    }
+}
+
+
+@Preview
+@Composable
+private fun PlantDetailContentPreview() {
+    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+    MdcTheme {
+        PlantDetailContent(plant)
+    }
+}
+
+@Preview
+@Composable
+private fun PlantWateringPreview() {
+    MdcTheme {
+        PlantWatering(7)
+    }
+}
+
+
+
+@Preview
+@Composable
 private fun PlantDescriptionPreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantDescription("HTML<br><br>description")
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PlantDetailContentDarkPreview() {
+    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+    MdcTheme {
+        PlantDetailContent(plant)
     }
 }
